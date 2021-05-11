5dbc0ec160acc674c9fec1e6100ed9141a30254b
tiennv0608 <tiennv0608@gmail.com>
2021-05-11 07:24:39 +0700

    b1

---
+++ 01_B1_ThuchanhAccessModifier/src/Simple.java
@@ -0,0 +1,20 @@
+class A1 {
+    private int data = 40;
+    private void msg(){
+        System.out.println("Hello Java");
+    }
+
+    public static void main(String[] args) {
+        A1 obj = new A1();
+        System.out.println(obj.data);
+        obj.msg();
+    }
+
+}
+//public class Simple {
+//    public static void main(String[] args) {
+//        A obj = new A();
+//        System.out.println(obj.data);
+//        obj.msg();
+//    }
+//}
